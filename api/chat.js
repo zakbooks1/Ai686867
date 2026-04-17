9e4a8489020190962b9321cd1435899a1c605440
zakbooks1 <zak.books1@gmail.com>
2026-04-17 07:53:52 -0500

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -18,7 +18,7 @@ export default async function handler(req, res) {
           "Content-Type": "application/json"
         },
         body: JSON.stringify({
-          model: "llama3-8b-8192",
+          model: "mixtral-8x7b-32768",
           messages: [
             { role: "user", content: body.message }
           ]
